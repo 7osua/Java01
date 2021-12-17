cfef6cbf40a1c067664f9d13405875ea44fb92f4
Josua Manullang <j7.manullang@gmail.com>
2021-12-18 01:33:17 +0700

    [+] Prototyping dengan membuat view dan model dasar

---
+++ src/TodoListApp.java
@@ -0,0 +1,32 @@
+public class TodoListApp {
+
+    public static String[] model = new String[7];
+
+    public static void getTodos() {
+
+    }
+
+    public static void addTodo(){
+
+    }
+
+    public static void delTodo(){
+
+    }
+
+    public static void viewTodos() {
+
+    }
+
+    public static void viewAddTodo() {
+
+    }
+
+    public static void viewDelTodo() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
