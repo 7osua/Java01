468596307a2a2cee783adb8aee830f10cf4b5414
Josua Manullang <j7.manullang@gmail.com>
2021-12-19 13:33:54 +0700

    [+] menambah dan bekerja dengan console. Menyelesaikan logic bisnis antara data, proses, logic bisnis untuk TodoList

--- README.md
+++ README.md
@@ -5,18 +5,23 @@ A simple todo list app. To grasp the basic of **Java**.
 So to start this mini project. This the list of task, <br>
 that I need to do and need to implement to get Up and Running.
 
-- [ ] Basic Java & Foundation To start.
-- [ ] Simple Prototyping.
+- [x] Basic Java & Foundation To start.
+- [x] Simple Prototyping.
+- [x] Get Input from User.
+- [x] Test the Get Input method.
 - [x] Show All Todo.
 - [x] Add Todo.
 - [x] Delete Todo.
 - [x] Testing for Show All todo.
 - [x] Testing for Add todo.
 - [x] Testing for Delete todo.
-- [ ] Create a **All todo View**.
-- [ ] Create a **Add todo View**.
-- [ ] Create a **Delete todo View**.
+- [x] Create a **All todo View**.
+- [x] Create a **Add todo View**.
+- [x] Create a **Delete todo View**.
+- [x] Test **All todo View**.
+- [x] Test **Add todo View**.
+- [x] Test **Delete todo View**.
 
-> 00:46:00
+> 00:00:00
 
 Start small :dart: Go solo :rocket:
--- src/TodoListApp.java
+++ src/TodoListApp.java
@@ -1,13 +1,98 @@
 public class TodoListApp {
 
     public static String[] todos = new String[7];
+    public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    public static String getInput(String description) {
+        System.out.print(description + "\t: ");
+        String todo = scanner.nextLine();
+        return todo;
+    }
+
+    public static void testGetInput() {
+       String aTodo = getInput("Ketik task mu");
+       System.out.println(aTodo);
+    }
+
+    public static void viewTodos() {
+        while(true){
+            String choosedMenu = "";
+
+            getTodos();
+
+            System.out.println("\nMenu\t:");
+            System.out.println("1.\t[+] ke List.");
+            System.out.println("2.\t[-] dari list.");
+            System.out.println("0.\t[x] keluar.\n");
+
+            choosedMenu = getInput("Pilih Salah satu");
+
+            if (choosedMenu.equals("1")) {
+                viewAddTodo();
+            } else if (choosedMenu.equals("2")) {
+                viewDelTodo();
+            } else if (choosedMenu.equals("0")) {
+                System.out.println("Thanks for trying.");
+                break;
+            } else {
+                System.out.println("Input tidak valid dengan Menu,\nPilih [1] atau [2].\n[0] Untuk keluar.\n");
+            }
+        }
+    }
+
+    public static void testViewTodos() {
+        addTodo("Ayo makan");
+        addTodo("Jangan lupa minum air");
+        addTodo("Tidur siang 15 menit");
+        viewTodos();
+    }
+
+    public static void viewAddTodo() {
+        String newTodo = getInput("\nKetikkan task baru.\n[0] Batal Menambah task.\n");
+
+        if (newTodo.equals("0")) {
+
+        } else {
+            addTodo(newTodo);
+        }
+    }
+
+    public static void testViewAddTodo() {
+        addTodo("Ayo makan");
+        addTodo("Jangan lupa minum air");
+        addTodo("Tidur siang 15 menit");
+        viewTodos();
+//        viewAddTodo();
+    }
+
+    public static void viewDelTodo() {
+        getTodos();
+        String todoId = getInput("\nMasukkan nomor [n] task.\n[0] Batal Menghapus task.\n");
+
+        if (todoId.equals("0")) {
+
+        } else {
+            boolean isSuccess = delTodo(Integer.valueOf(todoId));
+            if(!isSuccess) {
+                System.out.println("Gagal menghapus untuk\n" + "task id " + "[" + todoId + "]\n");
+            }
+        }
+    }
+
+    public static void testViewDelTodo() {
+        addTodo("Ayo makan");
+        addTodo("Jangan lupa minum air");
+        addTodo("Tidur siang 15 menit");
+        viewTodos();
+    }
 
     public static void getTodos() {
-        for(int i =0; i < todos.length; i++) {
+        System.out.println("\nTodolist saat ini\t:");
+        for (int i =0; i < todos.length; i++) {
             var todo = todos[i];
             var todoNo = i + 1;
 
-            if(todo != null ) {
+            if (todo != null ) {
                 System.out.println(todoNo + ". " + todo);
             }
         }
@@ -21,8 +106,8 @@ public class TodoListApp {
 
         var hasEmptyIndex = false;
 
-        for(int index = 0; index < todos.length; index++) {
-            if(todos[index] == null) {
+        for (int index = 0; index < todos.length; index++) {
+            if (todos[index] == null) {
                hasEmptyIndex = true;
                break;
             }
@@ -37,8 +122,8 @@ public class TodoListApp {
             }
         }
 
-        for(int index = 0; index < todos.length; index++) {
-            if(todos[index] == null) {
+        for (int index = 0; index < todos.length; index++) {
+            if (todos[index] == null) {
                 todos[index] = todo;
                 break;
             }
@@ -46,17 +131,17 @@ public class TodoListApp {
     }
 
     public static void testAddTodo() {
-        for(int index = 0; index < 7; index++) {
+        for (int index = 0; index < 7; index++) {
             addTodo("This todo task : " + (index + 1));
         }
     }
 
     public static boolean delTodo(Integer todoIndex){
-       if((todoIndex - 1) >= todos.length || todos[todoIndex - 1] == null) {
+       if ((todoIndex - 1) >= todos.length || todos[todoIndex - 1] == null) {
            return false;
        } else {
-           for(int index = (todoIndex - 1); index < todos.length; index++){
-               if(index == (todos.length - 1)) {
+           for (int index = (todoIndex - 1); index < todos.length; index++){
+               if (index == (todos.length - 1)) {
                    todos[index] = null;
                } else {
                    todos[index] = todos[index + 1];
@@ -72,21 +157,14 @@ public class TodoListApp {
         System.out.println(validDeleteTodo);
     }
 
-    public static void viewTodos() {
-
-    }
-
-    public static void viewAddTodo() {
-
-    }
-
-    public static void viewDelTodo() {
-
-    }
-
     public static void main(String[] args) {
-        testAddTodo();
-        testDelTodo();
-        testGetTodos();
+        viewTodos();
+//        testViewDelTodo();
+//        testViewAddTodo();
+//        testViewTodos();
+//        testGetInput();
+//        testDelTodo();
+//        testAddTodo();
+//        testGetTodos();
     }
 }
